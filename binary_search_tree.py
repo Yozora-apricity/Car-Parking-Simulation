aaecd3f7c5c4cb9371a4a4f2204db2d34bfc626a
Aira Villarito <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-12-22 23:58:10 +0800

    Add files via upload

---
+++ binary_search_tree.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+
+# ---------------- BST LOGIC ---------------- #
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
