7ada7f355ed79b10b234db5df4af072e3acc597f
Aira Villarito <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-12-23 02:32:06 +0800

    added the bst code with the traversals

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -7,6 +7,40 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+class BST:
+    def __init__(self):
+        self.root = None
 
-#BST UI     
+    def insert(self, value):
+        self.root = self._insert(self.root, value)
+
+    def _insert(self, node, value):
+        if not node:
+            return Node(value)
+        if value < node.value:
+            node.left = self._insert(node.left, value)
+        elif value > node.value:
+            node.right = self._insert(node.right, value)
+        return node
+
+    # LTR – Inorder
+    def ltr(self, node, result):
+        if node:
+            self.ltr(node.left, result)
+            result.append(node.value)
+            self.ltr(node.right, result)
+
+    # TLR – Preorder
+    def tlr(self, node, result):
+        if node:
+            result.append(node.value)
+            self.tlr(node.left, result)
+            self.tlr(node.right, result)
+
+    # LRT – Postorder
+    def lrt(self, node, result):
+        if node:
+            self.lrt(node.left, result)
+            self.lrt(node.right, result)
+            result.append(node.value)
         
