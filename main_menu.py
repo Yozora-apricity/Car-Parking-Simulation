78c9086eab0eaeeda36a1a1b92b574389048afa8
Gerald <geraldtrogado@gmail.com>
2025-12-29 11:12:42 +0800

    Creating a box ui for the class Main menu to serve as the function to call other files.

--- main_menu.py
+++ main_menu.py
@@ -1,2 +1,32 @@
+import tkinter as tk
+from tkinter import messagebox
+import os
+import sys
+import subprocess
+
 class MainMenu:
-    pass
+    def__init__(self, root):
+        self.root = root
+        self.root.title("Main Menu")
+        self.root.geometry("500x500")
+        self.root.configure(bg="black")
+        
+        self.center_window(500, 500)
+        
+        title_label = tk.Label(
+            self.root,
+            text="Main Menu",
+            font=("Arial", 23, "bold"),
+            fg="white",
+            bg="black"
+        )
+        title_label.pack(pady=(50, 10))
+
+        subtitle_label = tk.Label(
+            self.root, 
+            text="Select a program to launch:", 
+            font=("Arial", 12), 
+            bg="black", 
+            fg="white"
+        )
+        subtitle_label.pack(pady=(0, 40))
